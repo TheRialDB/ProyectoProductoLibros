b1eadd2f5f7cc813330c15c0ff81199264ad9902
david alejandro izquierdo cafaro <davidbeisbol@hotmail.com.ar>
2023-07-04 02:14:00 -0300

    modificacion capa frontal

--- GestorLibros.cs
+++ GestorLibros.cs
@@ -1,4 +1,5 @@
-﻿using Clases;
+﻿using CapaNegocios;
+using Clases;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,6 +15,57 @@ namespace ProyectoProductoLibros
 {
     public partial class GestorLibros : Form
     {
+        Libro NuevoLibro;
+        Libro LibroExistente;
+        NegLibro objNegLibro = new NegLibro();
+        bool nuevo = true;
+        int fila;
+
+        public GestorLibros()
+        {
+            InitializeComponent();
+            CrearDGV();
+            LlenarDGV();         
+        }
+
+        private void CrearDGV()
+        {
+            dgvLibros.Columns.Add("0", "Código");
+            dgvLibros.Columns.Add("1", "Título");
+            dgvLibros.Columns.Add("2", "Autor");
+            dgvLibros.Columns.Add("3", "Editorial");
+            dgvLibros.Columns.Add("4", "Género");
+            dgvLibros.Columns.Add("5", "Fecha de Emisión");
+
+            dgvLibros.Columns[0].Width = 100;
+            dgvLibros.Columns[1].Width = 100;
+            dgvLibros.Columns[2].Width = 100;
+            dgvLibros.Columns[3].Width = 100;
+            dgvLibros.Columns[4].Width = 100;
+            dgvLibros.Columns[5].Width = 100;
+        }
+
+        private void LlenarDGV()
+        {
+            dgvLibros.Rows.Clear();
+
+            DataSet ds = new DataSet();
+            ds = objNegLibro.listadoLibros("Todos");
+
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                foreach (DataRow dr in ds.Tables[0].Rows)
+                {
+                    //Lo que quieres mostrar esta en dr[0].ToString(), dr[1].ToString(),etc...
+                    dgvLibros.Rows.Add(dr[0].ToString(), dr[1], dr[2], dr[3], dr[4], dr[5].ToString());
+                }
+            }
+            else
+                MessageBox.Show("No hay Libros cargados en el sistema");
+        }
+
+ 
+    
         //creacion de listas para libros
         DataTable dtLibros = new DataTable() { TableName = "libros" };
 
@@ -23,65 +75,73 @@ namespace ProyectoProductoLibros
         //const string DIRECCION_XML = @"C:\Users\Usuario\OneDrive\Escritorio\FACU\PROGRAMACIÓN\vs2023\ProyectoProductoLibros\";
 
 
-        public GestorLibros()
-        {
-            InitializeComponent();
-
-            //columnas de mi tabla
-            dtLibros.Columns.Add("Código");
-            dtLibros.Columns.Add("Título");
-            dtLibros.Columns.Add("Autor");
-            dtLibros.Columns.Add("Editorial");
-            dtLibros.Columns.Add("Género");
-            dtLibros.Columns.Add("Emisión");
-
-            //Leer_DT();
-            dgvLibros.DataSource = dtLibros;
-        }
-
         private void btnCargar_Click(object sender, EventArgs e)
         {
-            Libro libro = new Libro();
+            #region prg viejo
+            //Libro libro = new Libro();
 
-            //limpieza del error provider
-            epvTextos.Clear();
+            ////limpieza del error provider
+            //epvTextos.Clear();
 
-            //validacion para llenar los campos
-            if (!Validar())
+            ////validacion para llenar los campos
+            //if (!Validar())
+            //{
+            //    int indice = cmbGenero.SelectedIndex;
+
+            //    libro.Codigo = Convert.ToInt32(txtCodigo.Text);
+            //    libro.Titulo = txtTitulo.Text;
+            //    libro.Autor = txtAutor.Text;
+            //    libro.Editorial = txtEditorial.Text;
+            //    libro.Genero = cmbGenero.Items[indice].ToString();
+
+            //    //creacion de nuevo libro con sus atributos
+            //    //Libro libro = new Libro(codigo, titulo, autor, editorial, genero, cantidad);
+
+
+            //    //validacion de codigo existente
+            //    int fila = BuscarCodigo(Convert.ToString(libro.Codigo));
+            //    if (fila != -1)
+            //    {
+            //        MessageBox.Show(ERROR_CODIGO_EXISTE);
+            //    }
+            //    else
+            //    {
+
+            //        //agregamos el nuevo libro a la lista
+            //        dtLibros.Rows.Add(new object[] { libro.Codigo, libro.Titulo, libro.Autor, libro.Editorial, libro.Genero });
+            //        //dtLibros.WriteXml(DIRECCION_XML + "librito.xml");
+
+            //        //limpieza de txts y cmb
+            //        txtCodigo.Clear();
+            //        txtTitulo.Clear();
+            //        txtAutor.Clear();
+            //        txtEditorial.Clear();
+            //        cmbGenero.SelectedIndex = -1;
+
+            //    }
+            //}
+            #endregion
+
+            int nGrabados = -1;
+
+            Libro NuevoLibro = new Libro();
+
+            nGrabados = objNegLibro.abmLibros("Alta", NuevoLibro);
+
+            if (nGrabados == -1)
             {
-                int indice = cmbGenero.SelectedIndex;
+                MessageBox.Show("No se pudo grabar el libro en el sistema");
+            }
+            else
+            {
+                NuevoLibro.Codigo = int.Parse(txtCodigo.Text);
+                NuevoLibro.Titulo = txtTitulo.Text;
+                NuevoLibro.Autor = txtAutor.Text;
+                NuevoLibro.Editorial = txtEditorial.Text;
+                NuevoLibro.Genero = cmbGenero.Text;
+                NuevoLibro.FechaEmision = dateTimePicker1.Value;
 
-                libro.Codigo = Convert.ToInt32(txtCodigo.Text);
-                libro.Titulo = txtTitulo.Text;
-                libro.Autor = txtAutor.Text;
-                libro.Editorial = txtEditorial.Text;                
-                libro.Genero = cmbGenero.Items[indice].ToString();
-
-                //creacion de nuevo libro con sus atributos
-                //Libro libro = new Libro(codigo, titulo, autor, editorial, genero, cantidad);
-
-
-                //validacion de codigo existente
-                int fila = BuscarCodigo(Convert.ToString(libro.Codigo));
-                if (fila != -1)
-                {
-                    MessageBox.Show(ERROR_CODIGO_EXISTE);
-                }
-                else
-                {
-
-                    //agregamos el nuevo libro a la lista
-                    dtLibros.Rows.Add(new object[] { libro.Codigo, libro.Titulo, libro.Autor, libro.Editorial, libro.Genero});
-                    //dtLibros.WriteXml(DIRECCION_XML + "librito.xml");
-
-                    //limpieza de txts y cmb
-                    txtCodigo.Clear();
-                    txtTitulo.Clear();
-                    txtAutor.Clear();
-                    txtEditorial.Clear();                   
-                    cmbGenero.SelectedIndex = -1;
-
-                }
+                LlenarDGV();
             }
         }
         private void btnBorrar_Click(object sender, EventArgs e)
@@ -170,11 +230,6 @@ namespace ProyectoProductoLibros
                 epvTextos.SetError(txtEditorial, "llenar campo");
                 validar = true;
             }
-            if (txtCantidad.Text == "")
-            {
-                epvTextos.SetError(txtCantidad, "llenar campo");
-                validar = true;
-            }
             if (cmbGenero.SelectedIndex == -1)
             {
                 epvTextos.SetError(cmbGenero, "llenar campo");
@@ -182,15 +237,5 @@ namespace ProyectoProductoLibros
             }
             return validar;
         }
-        //metodo para leer DT para saber si existe
-        //private void Leer_DT()
-        //{
-        //    if (System.IO.File.Exists(DIRECCION_XML + "librito.xml"))
-        //    {
-        //        dtLibros.ReadXml(DIRECCION_XML + "librito.xml");
-        //    }
-        //}
-
-        
     }
 }
