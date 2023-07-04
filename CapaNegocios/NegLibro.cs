e87ca78e8dcb2d13c26b0bf870b68a14bdd6db73
david alejandro izquierdo cafaro <davidbeisbol@hotmail.com.ar>
2023-07-04 00:48:32 -0300

    cambio capa negocio

---
+++ CapaNegocios/NegLibro.cs
@@ -0,0 +1,26 @@
+﻿using CapaDatos;
+using Clases;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocios
+{
+    public class NegLibro
+    {
+        AdminLibros DatosObjLibros = new AdminLibros();
+
+        public int abmLibros(string accion, Libro objLibro)
+        {
+            return DatosObjLibros.abmLibros(accion, objLibro);
+        }
+
+        public DataSet listadoLibros(string cual)
+        {
+            return DatosObjLibros.listadoLibros(cual);
+        }
+    }
+}
